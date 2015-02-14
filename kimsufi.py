2780cf263e570f7b1d92cf95556c0e6566cb86ea
Joris Vaillant <joris.vaillant@gmail.com>
2015-02-14 22:35:35 +0100

    Replace tab by space.

--- kimsufi.py
+++ kimsufi.py
@@ -49,88 +49,88 @@ ZONES = {'gra': 'Gravelines',
 
 
 def get_servers(models):
-	"""Get the servers from the OVH API."""
-	
-	r = requests.get(API_URL)
-	response = r.json()['answer']['availability']
-	
-	search = REFERENCES
-	if models:
-		search = {k: v for k, v in REFERENCES.items() if v in models}
-		
-	return [k for k in response if any(r == k['reference'] for r in search)]
+  """Get the servers from the OVH API."""
+
+  r = requests.get(API_URL)
+  response = r.json()['answer']['availability']
+
+  search = REFERENCES
+  if models:
+    search = {k: v for k, v in REFERENCES.items() if v in models}
+
+  return [k for k in response if any(r == k['reference'] for r in search)]
 
 
 def get_ref(name):
-	"""Return the reference based on the server model."""
-	
-	return list(REFERENCES.keys())[list(REFERENCES.values()).index(name)]
+  """Return the reference based on the server model."""
+
+  return list(REFERENCES.keys())[list(REFERENCES.values()).index(name)]
 
 
 def send_mail(output, total):
-	"""Send a mail to <MAIL_TO>."""
-	
-	subject = "{0} server{1} {2} available on Kimsufi".format(
-		total,
-		"s"[total<=1:],
-		["is", "are"][total>1]
-	)
-	headers = "From: {}\r\nTo: {}\r\nSubject: {}\r\n\r\n".format(
-		MAIL_FROM,
-		MAIL_TO,
-		subject
-	)
-	
-	try:
-		server = smtplib.SMTP(MAIL_HOST, MAIL_PORT)
-	except smtplib.socket.gaierror:
-		return False
-		
-	server.ehlo()
-	server.starttls()
-	server.ehlo()
-	
-	try:
-		server.login(MAIL_USERNAME, MAIL_PASSWORD)
-	except smtplib.SMTPAuthenticationError:
-		return False
-	
-	try:
-		server.sendmail(MAIL_FROM, MAIL_TO, headers + output)
-		return True
-	except Exception:
-		return False
-	finally:
-		server.close()
+  """Send a mail to <MAIL_TO>."""
+
+  subject = "{0} server{1} {2} available on Kimsufi".format(
+    total,
+    "s"[total<=1:],
+    ["is", "are"][total>1]
+  )
+  headers = "From: {}\r\nTo: {}\r\nSubject: {}\r\n\r\n".format(
+    MAIL_FROM,
+    MAIL_TO,
+    subject
+  )
+
+  try:
+    server = smtplib.SMTP(MAIL_HOST, MAIL_PORT)
+  except smtplib.socket.gaierror:
+    return False
+
+  server.ehlo()
+  server.starttls()
+  server.ehlo()
+
+  try:
+    server.login(MAIL_USERNAME, MAIL_PASSWORD)
+  except smtplib.SMTPAuthenticationError:
+    return False
+
+  try:
+    server.sendmail(MAIL_FROM, MAIL_TO, headers + output)
+    return True
+  except Exception:
+    return False
+  finally:
+    server.close()
 
 
 if __name__ == '__main__':
-	arguments = docopt(__doc__, version=VERSION)
-	kim = get_servers(arguments['<model>'])
+  arguments = docopt(__doc__, version=VERSION)
+  kim = get_servers(arguments['<model>'])
 
-	total = 0
-	output = ""
-	
-	for k in kim:
-		output += "\n{}\n".format(REFERENCES[k['reference']])
-		output += "{}\n".format("="*len(REFERENCES[k['reference']]))
-		
-		for z in k['zones']:
-			if z['availability'] == 'unavailable':
-				availability = z['availability']
-			else:
-				availability = "[OK]"
-				total += 1
-			output += '{} : {}\n'.format(ZONES[z['zone']], availability)
-	
-	output += "\n=======\nRESULT : {0} server{1} {2} available on Kimsufi\n=======\n".format(
-		total,
-		"s"[total<=1:],
-		["is", "are"][total>1]
-	)
-	
-	if total != 0 :
-		if arguments['--mail']:
-			send_mail(output, total)
-		else:
-			print(output)
+  total = 0
+  output = ""
+
+  for k in kim:
+    output += "\n{}\n".format(REFERENCES[k['reference']])
+    output += "{}\n".format("="*len(REFERENCES[k['reference']]))
+
+    for z in k['zones']:
+      if z['availability'] == 'unavailable':
+        availability = z['availability']
+      else:
+        availability = "[OK]"
+        total += 1
+      output += '{} : {}\n'.format(ZONES[z['zone']], availability)
+
+  output += "\n=======\nRESULT : {0} server{1} {2} available on Kimsufi\n=======\n".format(
+    total,
+    "s"[total<=1:],
+    ["is", "are"][total>1]
+  )
+
+  if total != 0 :
+    if arguments['--mail']:
+      send_mail(output, total)
+    else:
+      print(output)
